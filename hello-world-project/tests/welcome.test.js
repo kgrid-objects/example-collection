ec9174f2466a7d36f102b6fb8b50074579a95d8d
Greg Farris <greg.t.farris@gmail.com>
2018-08-13 12:50:55 -0400

    Added hello world test

---
+++ hello-world-project/tests/welcome.test.js
@@ -0,0 +1,16 @@
+var rewire = require('rewire');
+
+//Get load in the js
+var javascript = rewire('..//hello-world/v0.0.1/model/resource/welcome.js');
+
+//Load in the function
+var welcome = javascript.__get__("welcome");
+
+// noinspection BadExpressionStatementJS
+test('positive numbers', () => {
+
+  expect(welcome({"name": "Barney Rubble"})).toBe("Welcome to Knowledge Grid, Barney Rubble");
+
+});
+
+
