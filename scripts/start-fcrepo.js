8245b2b087b2b17f7670fa8875340d101bf5dc4e
Greg Farris <farrisg@umich.edus>
2018-10-25 16:32:06 -0400

    [#161303641] change postman script to use env and change docker name and added remove container on exit parameter

--- scripts/start-fcrepo.js
+++ scripts/start-fcrepo.js
@@ -1,9 +1,6 @@
-const newman = require('newman');
 const shell = require('shelljs');
 
-let cleanUp= shell.exec("docker rm fcrepo-it-test").stdout;
-
-let fcrepo = shell.exec("docker run --name fcrepo-it-test -p 8080:8080 kgrid/fcrepo:4.7.5" , function(code, stdout, stderr) {
+let fcrepo = shell.exec("docker run --rm --name fcrepo-it-test -p 8088:8080 kgrid/fcrepo:4.7.5" , function(code, stdout, stderr) {
   console.log('Exit code:', code);
   console.log('Program output:', stdout);
   console.log('Program stderr:', stderr);
