38f3fad8a908be0ecc63f2149407f51f19480fe5
Greg Farris <farrisg@umich.edus>
2018-10-28 19:34:33 -0400

    [#161303641] created KO and Implementation objects

--- scripts/start-fcrepo.js
+++ scripts/start-fcrepo.js
@@ -1,6 +1,6 @@
 const shell = require('shelljs');
 
-let fcrepo = shell.exec("docker run --rm --name fcrepo-it-test -p 8080:8080 kgrid/fcrepo:4.7.5" , function(code, stdout, stderr) {
+let fcrepo = shell.exec("docker run --rm --name fcrepo-test-it -p 8080:8080 kgrid/fcrepo:4.7.5" , function(code, stdout, stderr) {
   console.log('Exit code:', code);
   console.log('Program output:', stdout);
   console.log('Program stderr:', stderr);
