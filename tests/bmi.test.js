7b7ad989ec7be4dfdb9302a486af8578772c3c8f
Greg Farris <farrisg@umich.edus>
2018-11-19 15:25:32 -0500

    Duh.... fixed tests after changing location of KO objects

--- tests/bmi.test.js
+++ tests/bmi.test.js
@@ -1,7 +1,7 @@
 var rewire = require('rewire');
 
 //Get load in the js
-var javascript = rewire('../ri-bmicalc/v0.0.1/model/resource/bmi.js');
+var javascript = rewire('../collection/ri-bmicalc/v0.0.1/model/resource/bmi.js');
 
 //Load in the function
 var welcome = javascript.__get__("bmi_calc");
