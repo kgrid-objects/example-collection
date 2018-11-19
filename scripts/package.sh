d811475b925f6b239208acabb3d860ef8ba82e05
Greg Farris <farrisg@umich.edus>
2018-11-19 15:22:35 -0500

    Added collections folder for KO objects and added KOIO version for Hello World

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env sh
 
-# Setup
-rm -rf dist
-mkdir -p dist
-for i in $(ls -d *-*/); do cp -r ${i%%/} dist; done
-
-# create zip for each ko
-cd dist
+cd collection
 for d in * ; do
     echo "$d.zip"
-    zip -r -X "$d.zip" $d -x \"*.DS_Store\"
+    zip -r -X "../$d.zip" $d -x \"*.DS_Store\"
 done
 
-# clean up only leaving zip files
-rm -r */
 
