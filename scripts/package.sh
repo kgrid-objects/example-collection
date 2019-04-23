fa6ffd0b1f136a4e9f3c8cb587d8dc4c2fca0f99
gregfarris <farrisg@umich.edu>
2019-04-23 11:03:14 -0400

    [#165494081] update package scripts for objects

--- scripts/package.sh
+++ scripts/package.sh
@@ -4,9 +4,9 @@
 rm -rf dist
 mkdir -p dist
 
-(cd "collection/hello-world"; npm run package; )
-(cd "collection/exec-example"; npm run package; )
-(cd "collection/exec-step"; npm run package; )
+(cd "collection/hello-world"; npm install; npm run package; )
+(cd "collection/exec-example"; npm install; npm run package; )
+(cd "collection/exec-step";  npm install; npm run package; )
 (cd "collection"; zip -r -X "../dist/mvo-kgrid.zip" mvo-kgrid/ )
 (cd "collection"; zip -r -X "../dist/99999-fk4n99hh99.zip" 99999-fk4n99hh99/ )
 (cd "collection"; zip -r -X "../dist/ri-bmicalc.zip" ri-bmicalc/ )
