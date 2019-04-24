d62d1f8fb3cb635ca2a66bafaaa8a8f9493f192b
gregfarris <farrisg@umich.edu>
2019-04-24 11:55:43 -0400

    [#162387814] trying to run just one score/v0.2.0 all alond

--- scripts/package.sh
+++ scripts/package.sh
@@ -7,7 +7,6 @@ mkdir -p dist
 (cd "collection/hello-world"; npm install; npm run package; )
 (cd "collection/exec-example"; npm install; npm run package; )
 (cd "collection/exec-step";  npm install; npm run package; )
-(cd "collection/score-calc"; npm install; npm run build; npm run package; )
 (cd "collection"; zip -r -X "../dist/mvo-kgrid.zip" mvo-kgrid/ )
 (cd "collection"; zip -r -X "../dist/99999-fk4n99hh99.zip" 99999-fk4n99hh99/ )
 (cd "collection"; zip -r -X "../dist/ri-bmicalc.zip" ri-bmicalc/ )
