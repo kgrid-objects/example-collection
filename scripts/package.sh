6865cf3c828a4049153ee41535af03537e146964
George Meng <gqmeng.meng@gmail.com>
2019-11-20 13:35:14 -0500

    Updated build.sh

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 
 cd collection
-kgrid package ark:/hello/world
-kgrid package ark:/ri/bmicalc
-kgrid package ark:/score/calc
-kgrid package ark:/exec/step
-kgrid package ark:/exec/example
+kgrid package ark:/hello/world/v1.0
+kgrid package ark:/hello/world/v1.3
+kgrid package ark:/ri/bmicalc/v2.0
+kgrid package ark:/score/calc/v0.1.0
+kgrid package ark:/score/calc/v0.2.0
+kgrid package ark:/score/calc/v0.3.0
+kgrid package ark:/exec/step/v1.0.0
+kgrid package ark:/exec/example/v1.0.0
