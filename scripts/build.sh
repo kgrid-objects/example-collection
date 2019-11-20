fd9725b7b1787957eb2c10227bf8d2b005fcc862
George Meng <gqmeng.meng@gmail.com>
2019-11-20 13:39:37 -0500

    Updated postinstall.sh and build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-(cd ./collection/hello-world-v1.3; npm install)
-(cd ./collection/score-calc-v0.2.0; npm install)
-(cd ./collection/score-calc-v0.3.0; npm install)
-(cd ./collection/exec-example-v1.0.0; npm install)
-(cd ./collection/exec-step-v1.0.0; npm install)
+(cd ./collection/hello-world-v1.3; npm run build)
+(cd ./collection/score-calc-v0.2.0; npm run build)
+(cd ./collection/score-calc-v0.3.0; npm run build)
+(cd ./collection/exec-example-v1.0.0; npm run build)
+(cd ./collection/exec-step-v1.0.0; npm run build)
