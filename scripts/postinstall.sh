d8caf5b2a624143013c73441398e48af6f47bf0c
George Meng <gqmeng.meng@gmail.com>
2019-11-20 13:26:52 -0500

    Updated postinstall.sh

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-(cd ./collection/hello-world; npm install)
-(cd ./collection/score-calc; npm install)
-(cd ./collection/exec-example/v1.0.0; npm install)
-(cd ./collection/exec-step/v1.0.0; npm install)
+(cd ./collection/hello-world-v1.3; npm install)
+(cd ./collection/score-calc-v0.2.0; npm install)
+(cd ./collection/score-calc-v0.3.0; npm install)
+(cd ./collection/exec-example-v1.0.0; npm install)
+(cd ./collection/exec-step-v1.0.0; npm install)
