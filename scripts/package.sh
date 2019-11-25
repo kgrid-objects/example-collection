ebaf8662aac02c628fe3a5d1fe9853dd5a0adff0
George Meng <gqmeng.meng@gmail.com>
2019-11-25 14:50:27 -0500

    Updated package.sh and circleci config

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env sh
 
-cd collection
-kgrid package ark:/hello/world/v1.0
-kgrid package ark:/hello/world/v1.3
-kgrid package ark:/ri/bmicalc/v2.0
-kgrid package ark:/score/calc/v0.1.0
-kgrid package ark:/score/calc/v0.2.0
-kgrid package ark:/score/calc/v0.3.0
-kgrid package ark:/exec/step/v1.0.0
-kgrid package ark:/exec/example/v1.0.0
-kgrid package ark:/99999/fk4n99hh99/v0.0.2
-cd ..
+for dir in collection/* ;
+  do (cd "$dir" && kgrid package && cd ..)
+done
