615d7d0fbb1120c681f4199f5bb28abc9e3e0252
mradamgbeck <mradamgbeck@gmail.com>
2020-07-23 10:45:59 -0400

    added hellp world v8 edition

---
+++ collection/hello-worldV8-1.0/tests/bundled_welcome.test.js
@@ -0,0 +1,11 @@
+const welcome = require('../src/index');
+
+test('hello barney (src)', () => {
+  expect( welcome(
+      {"name":"Greg", "language": "Italian"}))
+           .toBe('Ciao, Greg')
+});
+
+
+
+
