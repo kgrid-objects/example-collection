843bffdd9abf3f48c050a1ab26b93b081e094817
mradamgbeck <mradamgbeck@gmail.com>
2020-10-23 15:21:32 -0400

    Fixed multimime objects

--- collection/python-multimime-v1.0/src/welcome.py
+++ collection/python-multimime-v1.0/src/welcome.py
@@ -3,9 +3,6 @@ import ast
 
 
 def welcome(user_input):
-    if type(user_input) == 'string':
-        input_object = ast.literal_eval(user_input)
-        return "Welcome to Knowledge Grid," + left_pad(input_object['name'], input_object['spaces'])
-    else:
-        return "Welcome to Knowledge Grid," + left_pad(user_input['name'], user_input['spaces'])
-
+    if isinstance(user_input, str):
+        user_input = ast.literal_eval(user_input)
+    return "Welcome to Knowledge Grid," + left_pad(user_input['name'], user_input['spaces'])
