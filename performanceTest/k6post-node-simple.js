07782e17500ff8d96346f25fe5c56cfb8754ca85
George Meng <gqmeng.meng@gmail.com>
2020-11-18 16:23:40 -0500

    Add additional k6 scripts

---
+++ performanceTest/k6post-node-simple.js
@@ -0,0 +1,19 @@
+import http from "k6/http";
+import {Counter} from "k6/metrics";
+
+let counter = new Counter("Number of 400");
+
+export default function() {
+  var url = "http://localhost:8080/node/simple/1.0/welcome";
+  // var url = "http://localhost:3000/kn101010101"
+  var payload = JSON.stringify({
+    name: "Steve"
+  });
+  var params =  { headers: { "Content-Type": "application/json" } }
+
+  let res = http.post(url, payload, params);
+
+  if(res.status === 400){
+    counter.add(1);
+  }
+};
