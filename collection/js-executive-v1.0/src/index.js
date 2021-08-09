103641c916ecc620844b38a9d4d6e2d3d594c352
ngittlen <nggittle@med.umich.edu>
2021-08-09 14:04:16 -0400

    Remove input stringify from V8 executive object code.

--- collection/js-executive-v1.0/src/index.js
+++ collection/js-executive-v1.0/src/index.js
@@ -2,7 +2,6 @@ function main(inputs) {
     var stepExecutor = context.getExecutor("js/simple/1.0/welcome");
 
     if (stepExecutor != null) {
-        inputs = JSON.stringify(inputs)
         return stepExecutor.execute(inputs, "application/json");
     } else {
         throw new Error("Cannot find simple js hello world ko.")
